5fe18e1d7ffd071811303cd62b97649712327344
Emily Reynolds <emilyreynoldsdsa@gmail.com>
2021-03-07 20:32:38 -0500

    Phrase repeater, repeats a phrase 100 times

---
+++ phraseToRepeat.py
@@ -0,0 +1,10 @@
+# A program to repeat a phrase 100 times
+
+#A phrase that will be repeated
+phraseToRepeat = "I will never spam my friends again."
+
+#A loop that prints a message once every time through the loop
+# 100 times. 
+for phraseAmount in range(100):
+    print(phraseToRepeat)
+    
