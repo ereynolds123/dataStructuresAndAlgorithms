c7f47830f36d3b55dc6150f669faa9adbd25049f
Emily Reynolds <emilyr@vermontsystems.com>
2021-03-24 07:42:00 -0400

    Added comments to recursive function.

--- recusionSum.py
+++ recusionSum.py
@@ -1,6 +1,9 @@
+# Takes an array 
 def listSum(numList):
+    #If array is 1 number long, return the value which is the total sum of the array
     if len(numList)==1:
         return numList[0]
+    #Otherwise, return the first value and recursively call for the second value in the array. 
     else:
         return numList[0]+ listSum(numList[1:])
     
