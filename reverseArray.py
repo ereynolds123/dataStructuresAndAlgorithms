4f75d8c5c7686dae86602c53ef63d40893c5168b
Emily Reynolds <emilyr@vermontsystems.com>
2021-03-26 14:35:04 -0400

    Final recursive array reversal

--- reverseArray.py
+++ reverseArray.py
@@ -1,13 +1,16 @@
 #Take an array and reverse the array
 def reverse(arr):
     fragment = ""
-    #First base case
+    #First base case if there is nothing in the array
     if len(arr)==0:
         return []
     
+    #Second base case if array has one item
     elif len(arr)==1:
         return arr[0]
     
+    #Recursively adds the final element of the fragment to the reversed fragment
+    #Turn fragment into a string to allow concatenation
     elif len(arr) > 1:
         reverseFragment = reverse(arr[1:])
         fragment = str(reverseFragment) + " " + str(arr[0])
