d6163c356c06d9819809a76775b34fa7e0111475
Emily Reynolds <emilyr@vermontsystems.com>
2021-04-28 08:55:41 -0400

    Printing grade in loop

---
+++ GPAcalculator.py
@@ -0,0 +1,17 @@
+#This program is a GPA calculator for my final Data Structures course
+
+#Accumulator variable for grades
+total_grades= 0
+
+#Add all grades to dictionary.
+#The first item of the key pair is the grade percentage, the second item is the possible total points
+grades_list = {98:15, 45:10, 89:50}
+
+def GPAcalculate(accumulator_Variable, dictionary):
+    for grade in dictionary:
+        print(grade)
+    
+    
+GPAcalculate(total_grades, grades_list)
+    
+    
