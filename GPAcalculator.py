598e1ab6f2c3b741881bd2eff3192f8b54341c49
Emily Reynolds <emilyr@vermontsystems.com>
2021-04-28 09:40:59 -0400

    Refactored cpde

--- GPAcalculator.py
+++ GPAcalculator.py
@@ -1,8 +1,7 @@
 #This program is a GPA calculator for my final Data Structures course
 
 #Accumulator variable for grades
-totalGrades= 0
-totalPotentialPoints =0
+#totalGrades= 0
 
 #Add all grades to dictionary.
 #The first item of the key pair is the grade percentage, the second item is the possible total points
@@ -11,20 +10,26 @@ gradesList = {98:15, 45:10, 89:50}
 #Function takes the accumulator variable and a dictionary
 #Calculates the total points of each pair
 #
-def GPA_calculate(accumulator_Variable, points_accumulator, dictionary):
+def GPA_calculate( dictionary):
     
     #Loops through the dictionary to get the key and value of the key pair
+    #Calculates the amount of earned points
     for key, value in dictionary.items():
         totalEarnedPoints = key * (value/100)
-        print(totalEarnedPoints)
-        
+    
+    print(totalEarnedPoints)
+    
+    #Finds the total potential grade points
     totalPotentialPoints = 0
     for value in dictionary.values():
         totalPotentialPoints = totalPotentialPoints +value
+    
     print(totalPotentialPoints)
+    
+    totalGPA = (totalEarnedPoints/totalPotentialPoints) * 100
+    print(totalGPA)
         
-        
-GPA_calculate(totalGrades, totalPotentialPoints, gradesList)
+GPA_calculate( gradesList)
 
     
     
