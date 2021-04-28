7e8fa7c1c21759fbb5058c4d33c8a6eb95675232
Emily Reynolds <emilyr@vermontsystems.com>
2021-04-28 09:28:20 -0400

    Printing out total earned points

--- GPAcalculator.py
+++ GPAcalculator.py
@@ -1,17 +1,30 @@
 #This program is a GPA calculator for my final Data Structures course
 
 #Accumulator variable for grades
-total_grades= 0
+totalGrades= 0
+totalPotentialPoints =0
 
 #Add all grades to dictionary.
 #The first item of the key pair is the grade percentage, the second item is the possible total points
-grades_list = {98:15, 45:10, 89:50}
+gradesList = {98:15, 45:10, 89:50}
 
-def GPAcalculate(accumulator_Variable, dictionary):
+#Function takes the accumulator variable and a dictionary
+#Calculates the total points of each pair
+#
+def GPA_calculate(accumulator_Variable, points_accumulator, dictionary):
+    
+    #Loops through the dictionary to get the key and value of the key pair
     for key, value in dictionary.items():
-        totalPoints = key * (value/100)
-        print(totalPoints)
+        totalEarnedPoints = key * (value/100)
+        print(totalEarnedPoints)
         
-GPAcalculate(total_grades, grades_list)
+    totalPotentialPoints = 0
+    for value in dictionary.values():
+        totalPotentialPoints = totalPotentialPoints +value
+    print(totalPotentialPoints)
+        
+        
+GPA_calculate(totalGrades, totalPotentialPoints, gradesList)
+
     
     
